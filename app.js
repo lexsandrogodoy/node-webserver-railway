4d6fcfd68d6b690de28b6cfccdbbe59c8c860631
Lexsandro Godoy <lexsandrogodoyg17@gmail.com>
2023-02-12 02:01:24 -0300

    Webserver 3

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     //res.sendFile( __dirname + '/public/index.html')
     res.render('home',{
-        nombre: 'Lexsandro Godoy',
+        nombre: 'Lexsandro Godoy G',
         titulo: 'Curso Node'
     });
 })
